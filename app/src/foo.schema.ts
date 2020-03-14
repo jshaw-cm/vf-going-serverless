3c9f716b0e3c7ac3b3cee1591c6a99f55a0df068
JusdM Shaw <JSHAW1@amfam.com>
2020-03-14 00:53:50 -0500

    feat: foo model and schema for dynamoose

---
+++ app/src/foo.schema.ts
@@ -0,0 +1,10 @@
+import { v4 as uuid } from 'uuid';
+import dynamoose from 'dynamoose';
+
+export const fooSchema = new dynamoose.Schema({
+  id: { type: String, required: true, hashKey: true, default: uuid },
+  foo: {
+    type: String,
+    required: true,
+  },
+});
