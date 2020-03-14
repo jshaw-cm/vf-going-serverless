f8c2ca9904b3ff4b7dc8d043676b61c8b99b614c
JusdM Shaw <JSHAW1@amfam.com>
2020-03-14 00:57:06 -0500

    feat: getFile; store record stubbed

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,35 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+import S3 from 'aws-sdk/clients/s3';
+import dynamoose from 'dynamoose';
+
+export interface FooBarInterface {
+  foo: string;
+}
+
+const getFile = async (key: string | undefined): Promise<FooBarInterface> => {
+  const s3 = new S3();
+  const options: S3.Types.GetObjectRequest = {
+    Bucket: process.env.BUCKET || 'undefined-bucket',
+    Key: key || 'undefined-key',
+  };
+
+  const file = await s3.getObject(options).promise();
+  return Buffer.from(file.Body?.toString()) as FooBarInterface;
+};
+
+const storeRecord = async (object: FooBarInterface): Promise<void> => {
+  const s3 = new S3();
+  const options: S3.Types.GetObjectRequest = {
+    Bucket: process.env.BUCKET || 'undefined-bucket',
+    Key: key || 'undefined-key',
+  };
+
+  const file = await s3.getObject(options).promise();
+  return Buffer.from(file.Body?.toString()) as FooBarInterface;
+};
+
+export const handler = async (): Promise<void> => {
+  try {
+  } catch (error) {
+    console.log(error);
   }
-  return a + b;
 };
