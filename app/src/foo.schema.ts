eb7e2115f0ba3c6920373c8d5acf11fc9b58340a
JusdM Shaw <JSHAW1@amfam.com>
2020-03-14 00:54:11 -0500

    style: lint

--- app/src/foo.schema.ts
+++ app/src/foo.schema.ts
@@ -2,7 +2,9 @@ import { v4 as uuid } from 'uuid';
 import dynamoose from 'dynamoose';
 
 export const fooSchema = new dynamoose.Schema({
-  id: { type: String, required: true, hashKey: true, default: uuid },
+  id: {
+    type: String, required: true, hashKey: true, default: uuid,
+  },
   foo: {
     type: String,
     required: true,
