2ea2e11d04e101e7a442abb2a5277b46f58dda6c
JusdM Shaw <JSHAW1@amfam.com>
2020-03-14 21:37:42 -0500

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es6: true,
+    node: true
   },
   extends: [
     'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:jest/recommended'
   ],
   globals: {
     Atomics: 'readonly',
@@ -17,9 +21,53 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
-    // 'jest'
+    'jest'
   ],
   rules: {
+    'import/extensions': [
+      "error",
+      "always",
+      {
+        ts: "never",
+        js: "never",
+      }
+    ],
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-new': 'off',
     'import/prefer-default-export': 'off',
   },
+  settings: {
+    'import/extensions': ['.js', '.ts'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts']
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts']
+      }
+    }
+  }
 };
+
+/**
+VS Code .vscode/settings.json
+From this guide: https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
+{
+  '[javascript]': {
+    'editor.formatOnSave': false
+  },
+  '[typescript]': {
+    'editor.formatOnSave': false
+  },
+  'eslint.autoFixOnSave': true,
+  'eslint.validate': [
+    'javascript',
+    {
+      'autoFix': true,
+      'language': 'typescript'
+    }
+  ],
+  'eslint.enable': true,
+}
+*/
