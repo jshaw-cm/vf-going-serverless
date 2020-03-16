dda8d226c4fc72697a31e1635f5dcf89fcf96653
JusdM Shaw <JSHAW1@amfam.com>
2020-03-16 10:35:34 -0500

    refactor: remove dynamosoe dydb setup

--- app/src/foo.schema.ts
+++ app/src/foo.schema.ts
@@ -1,12 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import dynamoose from 'dynamoose';
 
-dynamoose.setDDB({
-  config: {
-    endpoint: process.env.NODE_ENV === 'local' ? 'htt://localhost:4569' : undefined,
-  },
-});
-
 export const fooSchema = new dynamoose.Schema({
   id: {
     type: String,
