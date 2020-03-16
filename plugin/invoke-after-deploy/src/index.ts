ba1b322434945d0407c159dcedc062ae969f6a96
JusdM Shaw <JSHAW1@amfam.com>
2020-03-16 10:36:53 -0500

    refactor/docs: remove coments; enhance logging

--- plugin/invoke-after-deploy/src/index.ts
+++ plugin/invoke-after-deploy/src/index.ts
@@ -18,11 +18,6 @@ class InvokeAfterDeploy {
   options: Serverless.Options;
 
   constructor(serverless: Serverless, options: Serverless.Options) {
-    // eslint-disable-next-line constructor-super
-    // console.log('[SERVERLESS]', serverless);
-    // console.log('[options]', options);
-    // const functions = serverless.service.getAllFunctions();
-    // functions.forEach(f => console.log(serverless.service.getFunction(f)));
     this.serverless = serverless;
     this.options = options;
 
@@ -38,20 +33,33 @@ class InvokeAfterDeploy {
    * @memberof InvokeAfterDeploy
    */
   invoke(): void {
-    const { service } = this.serverless;
-    service.getAllFunctions().forEach(async f => {
-      const env = (service.getFunction(f).environment as unknown) as Environment;
-      if (env.INVOKE_AFTER_DEPLOY) {
-        const lambda = new Lambda({
-          region: this.options.region || 'us-east-1',
-        });
-        const options: InvocationRequest = {
-          FunctionName: `${service.getServiceName()}-${this.options.stage}-${f}`,
-        };
-        const invocation = await lambda.invoke(options).promise();
-        this.serverless.cli.log(this.log(invocation.StatusCode));
-      }
-    });
+    try {
+      // Pluck service out of this.serverless
+      const { service } = this.serverless;
+      // Get all functions
+      service.getAllFunctions().forEach(async f => {
+        const env = (service.getFunction(f).environment as unknown) as Environment;
+        // Check if lambda is needs to be invoked after deploy
+        if (env.INVOKE_AFTER_DEPLOY) {
+          // create lamba client, set region
+          const lambda = new Lambda({
+            region: this.options.region || 'us-east-1',
+          });
+          // Construct options for lamba invocation
+          const options: InvocationRequest = {
+            // parameterize Function name
+            FunctionName: `${service.getServiceName()}-${this.options.stage}-${f}`,
+          };
+          const invocation = await lambda.invoke(options).promise();
+          if (invocation.Payload)
+            this.serverless.cli.log(this.log(invocation.Payload.toString(), false));
+          else throw new Error('No response payload');
+        }
+      });
+    } catch (error) {
+      const e = { ...error } as Error;
+      this.serverless.cli.log(this.log(e.message, true));
+    }
   }
 
   /**
@@ -61,8 +69,14 @@ class InvokeAfterDeploy {
    * @memberof InvokeAfterDeploy
    */
   // eslint-disable-next-line class-methods-use-this
-  log(msg: string | number | undefined): string {
-    return `Invoke Function: ${chalk.yellow(msg)}`;
+  log(msg: string | number | undefined, error: boolean): string {
+    const prefix = 'Invoke Function: ';
+    switch (error) {
+      case true:
+        return `${prefix}${chalk.red(msg)}`; // Return red if error
+      default:
+        return `${prefix}${chalk.white(msg)}`; // Return white if no error
+    }
   }
 }
 
