fa9b830b4d426715abec49c6bd434caef8f2bffd
JusdM Shaw <JSHAW1@amfam.com>
2020-03-16 08:59:06 -0500

    refactor: move foo interface to its own file to avoid cycle

---
+++ app/src/foo.interface.ts
@@ -0,0 +1,4 @@
+export interface FooBarInterface {
+  id?: string;
+  foo: string;
+}
--- app/src/foo.model.ts
+++ app/src/foo.model.ts
@@ -1,8 +1,8 @@
 import dynamoose from 'dynamoose';
 import { fooSchema } from './foo.schema';
-import { FooBarInterface } from './index';
+import { FooBarInterface } from './foo.interface';
 
 export const FooModel = dynamoose.model<FooBarInterface, dynamoose.Schema>(
   process.env.TABLE || 'foo',
-  fooSchema,
+  fooSchema
 );
